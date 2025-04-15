42b4f66d6ab80bf348028ce136be0bfcde1fcd87
JDiegoHernandezA <jdiegohernandeza@gmail.com>
2025-04-15 11:49:48 -0500

    fix: se importa funcionalidad de CORS

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.18
 
 require (
 	github.com/gorilla/mux v1.8.1
-	github.com/joho/godotenv v1.5.1 
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 )
+
+require github.com/rs/cors v1.11.1
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/JuanDHernandezA/torneo_crud/config"
 	"github.com/JuanDHernandezA/torneo_crud/routes"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -18,6 +19,14 @@ func main() {
 	// Rutas
 	routes.SetupRoutes(r)
 
+	// Configurar CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:4200"}, // Permitir Angular
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
+
 	fmt.Println("🚀 Servidor corriendo en http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", c.Handler(r)))
 }
